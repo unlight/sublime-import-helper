f4eb23864510ee4d31957a9a0c05a653d9ef16b4
iamthes <s91630277@gmail.com>
2017-01-30 18:12:31 +0400

    Fixed errors when broken package.json

--- backend/commands/get_packages.js
+++ backend/commands/get_packages.js
@@ -3,12 +3,18 @@ const _keys = require("lodash.keys");
 const _flatten = require("lodash.flatten");
 const readPkgUp = require("read-pkg-up");
 
+const emptyPkg = {
+    dependencies: [],
+    devDependencies: []
+};
+
 module.exports = (data, callback) => {
     var folderList = data.folders || [];
     var requests = folderList.map(d => esm.directory(d));
     var importRoot = data.importRoot;
     if (importRoot) {
         var npmModules = readPkgUp({ cwd: importRoot, normalize: false })
+            .catch(() => Promise.resolve({pkg: emptyPkg}))
             .then(p => [_keys(p.pkg.dependencies), _keys(p.pkg.devDependencies)])
             .then(dependencies => _flatten(dependencies))
             .then(names => Promise.all(names.map(n => esm.parseModule(n, {dirname: importRoot}))))
--- backend/test.js
+++ backend/test.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import * as Path from 'path';
 const pkgDir = require('pkg-dir');
 var rootPath;
 var _state = { packages: [] };
@@ -20,6 +21,7 @@ test.cb('Ping', t => {
 });
 
 var readPackagesCmd = require('./commands/read_packages');
+var getPackagesCmd = require('./commands/get_packages');
 
 test.cb('Read packages', t => {
     var projectDirectory = rootPath;
@@ -46,6 +48,59 @@ test.cb('Read packages projectDirectory null', t => {
     }, (err, response) => {
         t.truthy(err);
         t.falsy(response);
+        t.end();
+    });
+});
+
+test.cb('Get packages no pkg', t => {
+    var importRoot = Path.join(rootPath, 'test_playground/no_pkg');
+    getPackagesCmd({
+        _state: _state,
+        folders: [],
+        importRoot: importRoot
+    }, (err, response) => {
+        t.falsy(err);
+        t.truthy(response);
+        t.truthy(response.length > 0);
+        t.end();
+    });
+});
+
+test.cb('Get packages with broken json', t => {
+    var importRoot = Path.join(rootPath, 'test_playground/bad_json');
+    getPackagesCmd({
+        _state: _state,
+        folders: [],
+        importRoot: importRoot
+    }, (err, response) => {
+        t.falsy(err);
+        t.deepEqual(response, []);
+        t.end();
+    });
+});
+
+test.cb('Get packages with empty_pkg', t => {
+    var importRoot = Path.join(rootPath, 'test_playground/empty_pkg');
+    getPackagesCmd({
+        _state: _state,
+        folders: [],
+        importRoot: importRoot
+    }, (err, response) => {
+        t.falsy(err);
+        t.deepEqual(response, []);
+        t.end();
+    });
+});
+
+test.cb('Get packages with empty_file', t => {
+    var importRoot = Path.join(rootPath, 'test_playground/empty_file');
+    getPackagesCmd({
+        _state: _state,
+        folders: [],
+        importRoot: importRoot
+    }, (err, response) => {
+        t.falsy(err);
+        t.deepEqual(response, []);
         t.end();
     });
 });
