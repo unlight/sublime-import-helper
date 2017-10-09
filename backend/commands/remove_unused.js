04ce980deae21444c795bb3728d7d3bef8025a26
roman.vasilev <>
2017-10-09 17:23:24 +0400

    Fixed remove regexp

--- backend/commands/remove_unused.js
+++ backend/commands/remove_unused.js
@@ -1,9 +1,9 @@
 var spawn = require('cross-spawn');
 
 module.exports = (data, callback) => {
-    var file_name = data.file_name;
+    var file_name = data.file_name.replace(/\\/g, '/');
     var options = { encoding: 'utf8' , cwd: data.cwd };
-    var proc = spawn('node_modules/.bin/tsc', [ file_name, '--noEmit', '--pretty', 'false', '--noUnusedLocals', '--allowJs'], options);
+    var proc = spawn('node_modules/.bin/tsc', [file_name, '--noEmit', '--pretty', 'false', '--noUnusedLocals', '--allowJs'], options);
     proc.on('error', (err) => {
         callback(err);
     });
@@ -14,18 +14,22 @@ module.exports = (data, callback) => {
         var result = {};
         for (var i = 0; i < outlines.length; i++) {
             var line = outlines[i];
-            var match = line.match(/^.+\((\d+),(\d+)\): error TS(\d+): '(\w+)'/);
-            if (!match || match[3] !== '6133') continue;
-            var line = Number(match[1]);
-            var pos = Number(match[2]);
-            var name = match[4];
+            // TODO: Need take into account file
+            var match = line.match(/^(.+)\((\d+),(\d+)\): error TS(\d+): '(\w+)'/);
+            if (!match || match[4] !== '6133') continue;
+            var file = match[1].replace(/\\/g, '/');
+            if (file_name.slice(-file.length) !== file) continue;
+            var line = Number(match[2]);
+            var pos = Number(match[3]);
+            var name = match[5];
             if (!Array.isArray(result[line])) {
                 result[line] = [];
             }
             result[line].push({
-                line: Number(match[1]),
-                pos: Number(match[2]),
-                name: match[4],
+                file: file,
+                line: line,
+                pos: pos,
+                name: name,
             });
         }
         callback(null, result);
