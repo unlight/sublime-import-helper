3e665cd2fe2517848f91b722f3546e46186fa188
roman.vasilev <>
2017-10-19 19:28:01 +0400

    Define project_settings above check

--- do_insert_import.py
+++ do_insert_import.py
@@ -12,6 +12,7 @@ class DoInsertImportCommand(sublime_plugin.TextCommand):
         super().__init__(view)
         self.user_settings = sublime.load_settings('import_helper')
         project_data = sublime.active_window().project_data()
+        self.project_settings = {}
         if project_data is not None:
             self.project_settings = project_data.get('import_helper') or {}
 
