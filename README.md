b11a5de3797b251a76b2891b5d22e6164cfe6d14
roman <>
2020-06-05 14:34:53 +0400

    feat: Removed unused imports command
    BREAKING CHANGE:
    Removed unused imports command, the replacement is eslint/tslint rules with fixers https://github.com/cartant/tslint-etc (no-unused-declaration)

--- README.md
+++ README.md
@@ -1,122 +1,159 @@
 # sublime-import-helper
+
 A Sublime Text Plugin that helps you to import your modules.
 
 ## Supported Languages
-* TypeScript
-* JavaScript (ES2015)
+
+-   TypeScript
+-   JavaScript (ES2015)
 
 ## Requirements
-* Node.JS 6.0+
+
+-   Node.JS 6.0+
 
 ## Installation
+
 #### [PackageControl](https://packagecontrol.io/packages/ImportHelper)
-* Select `Package Control: Install Package` from command palette
-* Select `ImportHelper`
+
+-   Select `Package Control: Install Package` from command palette
+-   Select `ImportHelper`
 
 #### Manual Installation
+
 You can install `sublime-import-helper` manually using git by running the following command
 within sublime packages directory (Preferences > Browse Packages):
+
 ```
 git clone https://github.com/unlight/sublime-import-helper ImportHelper
 ```
 
 ## Usage
+
 #### Initialize / Setup / Update modules
-* Restart plugin - update node_modules, source modules
+
+-   Restart plugin - update node_modules, source modules
 
 #### Insert import
-* Set cursor or select word
-* Press `ctrl+alt+i`, or select the command from command palette
+
+-   Set cursor or select word
+-   Press `ctrl+alt+i`, or select the command from command palette
 
 #### List imports
-* Press `alt+i, alt+l`, or select the command from command palette
+
+-   Press `alt+i, alt+l`, or select the command from command palette
 
 #### Update source modules
-* Press `alt+i, alt+s`, or select the command from command palette
+
+-   Press `alt+i, alt+s`, or select the command from command palette
 
 #### Import from clipboard
-* Copy text to clipboard `ctrl+c`
-* Press `alt+i, alt+k`, or select the command from command palette
 
-#### Remove unused imports (TypeScript only)
-* Press `alt+i, alt+u`, or select the command from command palette
+-   Copy text to clipboard `ctrl+c`
+-   Press `alt+i, alt+k`, or select the command from command palette
 
 ## Screenshots
+
 ![](https://raw.githubusercontent.com/unlight/sublime-import-helper/master/screenshots/insert-import.gif)
 
 ## Settings
+
 There are some several configuration settings. Open plugin settings file by opening in menu:  
 Preferences -> Package Settings -> Import Helper  
 Also, there are some optional project specific settings.  
 The precedence of getting of value of setting is following:
+
 1. Project file
 2. Plugin file settings
 3. Default settings
 
 #### `insert_position`
+
 Specifies where new import statement should be inserted, at the beginning ('start')
 or at the end of imports block ('end').
-- Type: `string`
-- Enum: `['end', 'start']`
-- Default: `end`
+
+-   Type: `string`
+-   Enum: `['end', 'start']`
+-   Default: `end`
 
 #### `from_quote`
+
 What kind of quotes will be used in import statement.
-- Type: `string`
-- Default: `'`
+
+-   Type: `string`
+-   Default: `'`
 
 #### `space_around_braces`
+
 Paste space before opening and after closing curly brackets.
-- Type: `boolean`
-- Default: `true`
+
+-   Type: `boolean`
+-   Default: `true`
 
 #### `node_bin`
+
 Sometimes sublime cannot find node executable, if it happens. Set `node_bin` explicitly (e.g. c:/nodejs/node.exe)
-- Type: `string`
-- Default: `` (auto detect)
+
+-   Type: `string`
+-   Default: `` (auto detect)
 
 #### `from_semicolon`
-Add semicolon to the end of `import` string. 
-- Type: `boolean`
-- Default: `true`
+
+Add semicolon to the end of `import` string.
+
+-   Type: `boolean`
+-   Default: `true`
 
 #### `import_path_mapping`
+
 How to apply path mapping (read more about [Module Resolution and Path Mapping](http://www.typescriptlang.org/docs/handbook/module-resolution.html)).  
 Disabled by default (`disabled`).  
-If `enabled` implementation will try to find first matching alias.  
-- Type: `string`  
-- Enum: `['disabled', 'enabled']`  
-- Default: `disabled`  
+If `enabled` implementation will try to find first matching alias.
+
+-   Type: `string`
+-   Enum: `['disabled', 'enabled']`
+-   Default: `disabled`
 
 #### `autocomplete_export_names`
-Show all possible export names from sources and node modules in autocomplete menu.  
-- Type: `boolean`  
-- Default: `true`
+
+Show all possible export names from sources and node modules in autocomplete menu.
+
+-   Type: `boolean`
+-   Default: `true`
 
 #### `autocomplete_auto_import`
+
 Automatically add import statement if export name was selected from autocomplete menu (Ctrl + Space).  
-Requires `autocomplete_export_names: true`.  
-- Type: `boolean`  
-- Default: `false`
+Requires `autocomplete_export_names: true`.
+
+-   Type: `boolean`
+-   Default: `false`
 
 #### `remove_trailing_index`
-Remove index suffix ending in file path  
-- Type: `boolean`  
-- Default: `true`  
+
+Remove index suffix ending in file path
+
+-   Type: `boolean`
+-   Default: `true`
 
 #### `import_no_match_count`
-Usually imports are at the top of the file, this settings controls how far countinue scan lines  
-- Type: `number`  
-- Default: `15`  
+
+Usually imports are at the top of the file, this settings controls how far countinue scan lines
+
+-   Type: `number`
+-   Default: `15`
 
 #### `import_root` (project file only)
+
 Path to your project root folder (not source folder). If not set, `folders[0].path` will be used.
 
 #### `folders[i].path_source` (project file only)
+
 Path to your source. If not set `folders[i].path` will be used.
 
 #### Example of settings in project file:
+
 Example of project file:
+
 ```
 {
 	"import_root": ".",
@@ -126,22 +163,26 @@ Example of project file:
 			"path": "."
 		}
 	]
-} 
+}
 ```
 
 ## FAQ
+
 #### No imports found for...
+
 Or if you see such message in status bar and console:
+
 ```
 There is no project file, ... will not work without project.
 ```
+
 You must create project from your working files and folders, you can do it in top menu:  
 `Project -> Save project as...`  
 Save project file in any place you want.  
-Then restart Sublime.  
-
+Then restart Sublime.
 
 ## Development
+
 ```python
 # sublime.log_input(True); sublime.log_commands(True); sublime.log_result_regex(True)
 # sublime.log_input(False); sublime.log_commands(False); sublime.log_result_regex(False)
@@ -149,7 +190,8 @@ python3 -m black .
 ```
 
 ## TODO
-* move all commands to main file
-* parse single file
-* handle all selections
-* watch for project file changes
+
+-   move all commands to main file
+-   parse single file
+-   handle all selections
+-   watch for project file changes
--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,12 @@
 import sublime
 import sys
 from unittest import TestCase
-import_helper = sys.modules['ImportHelper.import_helper']
-utils = sys.modules['ImportHelper.utils']
+
+import_helper = sys.modules["ImportHelper.import_helper"]
+utils = sys.modules["ImportHelper.utils"]
+
 
 class TestDoInsertImport(TestCase):
-    
     def setUp(self):
         self.view = sublime.active_window().new_file()
         s = sublime.load_settings("Preferences.sublime-settings")
@@ -21,38 +22,61 @@ class TestDoInsertImport(TestCase):
         return self.view.substr(self.view.line(self.view.text_point(row, 0)))
 
     def getAll(self):
-        return self.view.substr(sublime.Region(0, self.view.size()));
+        return self.view.substr(sublime.Region(0, self.view.size()))
 
     def test_smoke(self):
-        setText(self.view, '')
-        self.view.run_command('paste_import', {'item': {'filepath': 'dinah_widdoes', 'name': 'Lakia', 'isDefault': False}})
+        setText(self.view, "")
+        self.view.run_command(
+            "paste_import",
+            {
+                "item": {
+                    "filepath": "dinah_widdoes",
+                    "name": "Lakia",
+                    "isDefault": False,
+                }
+            },
+        )
         first_row = self.getRow(0)
-        self.assertTrue(first_row.startswith('import {'))
-        self.assertIn('}', first_row)
-        self.assertIn('Lakia', first_row)
-        self.assertIn('dinah_widdoes', first_row)
-        self.assertFalse(first_row.endswith(';'))
+        self.assertTrue(first_row.startswith("import {"))
+        self.assertIn("}", first_row)
+        self.assertIn("Lakia", first_row)
+        self.assertIn("dinah_widdoes", first_row)
+        self.assertFalse(first_row.endswith(";"))
 
     def test_side_effect_import(self):
-        setText(self.view, 'import "rxjs/operators/map"\n');
-        self.view.run_command('paste_import', {'item': {'filepath': 'side/effect', 'name': 'effect', 'isDefault': False}})
-        self.assertIn('./side/effect', self.getRow(1))
+        setText(self.view, 'import "rxjs/operators/map"\n')
+        self.view.run_command(
+            "paste_import",
+            {"item": {"filepath": "side/effect", "name": "effect", "isDefault": False}},
+        )
+        self.assertIn("./side/effect", self.getRow(1))
 
     def test_paste_import_if_imports_statements_in_the_middle(self):
-        setText(self.view, ('\n' * 14) + 'import x from "x"\n');
-        self.view.run_command('paste_import', {'item': {'filepath': 'filepath', 'name': 'name', 'isDefault': False}})
-        self.assertNotIn('./filepath', self.getRow(0))
-        self.assertNotIn('name', self.getRow(0))
+        setText(self.view, ("\n" * 14) + 'import x from "x"\n')
+        self.view.run_command(
+            "paste_import",
+            {"item": {"filepath": "filepath", "name": "name", "isDefault": False}},
+        )
+        self.assertNotIn("./filepath", self.getRow(0))
+        self.assertNotIn("name", self.getRow(0))
 
     def test_add_specifier_to_default_import(self):
-        setText(self.view, "import React from 'react'\n");
-        self.view.run_command('paste_import', {'item': {'name': 'useCallback', 'module': 'react', 'isDefault': False}})
+        setText(self.view, "import React from 'react'\n")
+        self.view.run_command(
+            "paste_import",
+            {"item": {"name": "useCallback", "module": "react", "isDefault": False}},
+        )
         self.assertIn("import React, { useCallback } from 'react", self.getRow(0))
 
     def test_add_specifier_to_mixed_import(self):
-        setText(self.view, "import React, { useCallback } from 'react'\n");
-        self.view.run_command('paste_import', {'item': {'name': 'useState', 'module': 'react', 'isDefault': False}})
-        self.assertIn("import React, { useCallback, useState } from 'react", self.getRow(0))
+        setText(self.view, "import React, { useCallback } from 'react'\n")
+        self.view.run_command(
+            "paste_import",
+            {"item": {"name": "useState", "module": "react", "isDefault": False}},
+        )
+        self.assertIn(
+            "import React, { useCallback, useState } from 'react", self.getRow(0)
+        )
 
     # These tests shows additional popup
     # def test_typescript_paths(self):
@@ -81,22 +105,50 @@ class TestDoInsertImport(TestCase):
     #     self.assertIn("import {koo} from '@components'", self.getRow(0))
 
     def test_remove_importpath_index(self):
-        setText(self.view, '')
-        self.view.run_command('paste_import', {'item': {'filepath': './component/x/index', 'name': 'x1', 'isDefault': False}})
+        setText(self.view, "")
+        self.view.run_command(
+            "paste_import",
+            {
+                "item": {
+                    "filepath": "./component/x/index",
+                    "name": "x1",
+                    "isDefault": False,
+                }
+            },
+        )
         self.assertIn("import {x1} from './component/x'", self.getRow(0))
-        self.view.run_command('paste_import', {'item': {'filepath': './component/x/index', 'name': 'x2', 'isDefault': False}})
+        self.view.run_command(
+            "paste_import",
+            {
+                "item": {
+                    "filepath": "./component/x/index",
+                    "name": "x2",
+                    "isDefault": False,
+                }
+            },
+        )
         self.assertIn("import {x1, x2} from './component/x'", self.getRow(0))
 
     def test_paste_import_module(self):
-        setText(self.view, '')
-        self.view.run_command('paste_import', {'item': {'module': '@angular/core', 'specifier': './debug/debug_node', 'isDefault': False, 'name': 'Inject'}})
+        setText(self.view, "")
+        self.view.run_command(
+            "paste_import",
+            {
+                "item": {
+                    "module": "@angular/core",
+                    "specifier": "./debug/debug_node",
+                    "isDefault": False,
+                    "name": "Inject",
+                }
+            },
+        )
         self.assertIn("import {Inject} from '@angular/core'", self.getRow(0))
 
-class TestInitializeSetup(TestCase):
 
+class TestInitializeSetup(TestCase):
     def setUp(self):
         self.window = sublime.active_window()
-        self.window.run_command('initialize_setup')
+        self.window.run_command("initialize_setup")
 
     def test_check_node_modules(self):
         yield 5000
@@ -107,104 +159,120 @@ class TestInitializeSetup(TestCase):
         self.assertNotEqual(len(import_helper.source_modules), 0)
 
     def test_exclude_should_work(self):
-        ignored = [item for item in import_helper.source_modules if 'ignored' in item['filepath']]
+        ignored = [
+            item
+            for item in import_helper.source_modules
+            if "ignored" in item["filepath"]
+        ]
         self.assertEqual(len(ignored), 0)
 
-class TestUtilFunctions(TestCase):
 
+class TestUtilFunctions(TestCase):
     def test_debug_disabled(self):
         self.assertFalse(import_helper.DEBUG)
 
     def test_run_path_should_point_to_debug_version(self):
         run_path = import_helper.RUN_PATH
-        self.assertIn('backend_run', run_path)
+        self.assertIn("backend_run", run_path)
 
     def test_unixify(self):
         unixify = import_helper.unixify
-        _ = '\\local\\some\\file'
-        self.assertTrue(unixify(_) == '/local/some/file')
-        
+        _ = "\\local\\some\\file"
+        self.assertTrue(unixify(_) == "/local/some/file")
+
     def test_unixify_ts(self):
         unixify = import_helper.unixify
-        _ = 'some\\file.ts'
-        self.assertTrue(unixify(_) == 'some/file')
-        
+        _ = "some\\file.ts"
+        self.assertTrue(unixify(_) == "some/file")
+
     def test_unixify_tsx(self):
         unixify = import_helper.unixify
-        path = 'd/file.tsx'
-        self.assertTrue(unixify(path) == 'd/file')
+        path = "d/file.tsx"
+        self.assertTrue(unixify(path) == "d/file")
 
     def test_unixify_js(self):
         unixify = import_helper.unixify
-        _ = 'some\\file.js'
-        self.assertTrue(unixify(_) == 'some/file')
+        _ = "some\\file.js"
+        self.assertTrue(unixify(_) == "some/file")
 
     def test_is_excluded_file(self):
         is_excluded_file = import_helper.is_excluded_file
-        self.assertTrue(is_excluded_file('dir/file1.ts', ['*.ts']))
-        self.assertTrue(is_excluded_file('dir1/file1.ts', ['dir1']))
+        self.assertTrue(is_excluded_file("dir/file1.ts", ["*.ts"]))
+        self.assertTrue(is_excluded_file("dir1/file1.ts", ["dir1"]))
 
     def test_get_setting(self):
         get_setting = import_helper.get_setting
-        self.assertEqual(get_setting('insert_position', None), 'end')
-        self.assertEqual(get_setting('from_quote', None), "'")
-        self.assertEqual(get_setting('space_around_braces', None), False)
-        self.assertEqual(get_setting('from_semicolon', True), False)
-        self.assertEqual(get_setting('unknown', 'default_value'), 'default_value')
+        self.assertEqual(get_setting("insert_position", None), "end")
+        self.assertEqual(get_setting("from_quote", None), "'")
+        self.assertEqual(get_setting("space_around_braces", None), False)
+        self.assertEqual(get_setting("from_semicolon", True), False)
+        self.assertEqual(get_setting("unknown", "default_value"), "default_value")
 
     def test_find_executable(self):
-        result = utils.find_executable('node')
-        self.assertIn('node.exe', result)
+        result = utils.find_executable("node")
+        self.assertIn("node.exe", result)
 
     def test_get_import_root(self):
         get_import_root = import_helper.get_import_root
         result = get_import_root()
-        self.assertTrue('ImportHelper' in result)
+        self.assertTrue("ImportHelper" in result)
 
     def test_get_panel_item_negative_test(self):
         get_panel_item = import_helper.get_panel_item
-        result = get_panel_item('/', {})
+        result = get_panel_item("/", {})
         self.assertTrue(result is None)
 
     def test_query_completions_modules(self):
         query_completions_modules = import_helper.query_completions_modules
         source_modules = [
-            {'name': 'good', 'filepath':'/usr/home/good'},
-            {'name': 'ugly', 'filepath':'/usr/home/ugly'}
+            {"name": "good", "filepath": "/usr/home/good"},
+            {"name": "ugly", "filepath": "/usr/home/ugly"},
         ]
-        node_modules = [
-            {'name': 'Chicky', 'module': 'chicken'}
-        ]
-        result = query_completions_modules('goo', source_modules, node_modules)
-        self.assertListEqual(result, [['good\tsource_modules', 'good']])
-        result = query_completions_modules('Chic', source_modules, node_modules)
-        self.assertListEqual(result, [['Chicky\tnode_modules/chicken', 'Chicky']])
+        node_modules = [{"name": "Chicky", "module": "chicken"}]
+        result = query_completions_modules("goo", source_modules, node_modules)
+        self.assertListEqual(result, [["good\tsource_modules", "good"]])
+        result = query_completions_modules("Chic", source_modules, node_modules)
+        self.assertListEqual(result, [["Chicky\tnode_modules/chicken", "Chicky"]])
 
     def test_get_exclude_patterns_fault_tollerance(self):
         get_exclude_patterns = import_helper.get_exclude_patterns
-        result = get_exclude_patterns({'folders': {}})
+        result = get_exclude_patterns({"folders": {}})
         self.assertDictEqual(result, {})
 
     def test_is_import_all(self):
-        self.assertTrue(utils.is_import_all("import * as worker_threads from 'worker_threads'"))
-        self.assertFalse(utils.is_import_all("import worker_threads from 'worker_threads'"))
+        self.assertTrue(
+            utils.is_import_all("import * as worker_threads from 'worker_threads'")
+        )
+        self.assertFalse(
+            utils.is_import_all("import worker_threads from 'worker_threads'")
+        )
 
     def test_is_import_default(self):
-        self.assertFalse(utils.is_import_default("import * as worker_threads from 'worker_threads'"))
-        self.assertTrue(utils.is_import_default("import worker_threads from 'worker_threads'"))
+        self.assertFalse(
+            utils.is_import_default("import * as worker_threads from 'worker_threads'")
+        )
+        self.assertTrue(
+            utils.is_import_default("import worker_threads from 'worker_threads'")
+        )
         self.assertTrue(utils.is_import_default("import React from 'react'"))
-        self.assertFalse(utils.is_import_default("import React, { useState } from 'react'"))
+        self.assertFalse(
+            utils.is_import_default("import React, { useState } from 'react'")
+        )
 
     def test_is_import_mixed(self):
         self.assertFalse(utils.is_import_mixed("import React from 'react'"))
-        self.assertTrue(utils.is_import_mixed("import React, { useState } from 'react'"))
-        self.assertTrue(utils.is_import_mixed("import React, { useState, useCallback } from 'react'"))
+        self.assertTrue(
+            utils.is_import_mixed("import React, { useState } from 'react'")
+        )
+        self.assertTrue(
+            utils.is_import_mixed(
+                "import React, { useState, useCallback } from 'react'"
+            )
+        )
 
-class TestUnsedImports(TestCase):
-
+class TestPasteImport(TestCase):
     def setUp(self):
         self.view = sublime.active_window().new_file()
-        # make sure we have a window to work with
         s = sublime.load_settings("Preferences.sublime-settings")
         s.set("close_windows_when_empty", False)
 
@@ -214,69 +282,11 @@ class TestUnsedImports(TestCase):
             self.view.window().focus_view(self.view)
             self.view.window().run_command("close_file")
 
-    def getRow(self, row):
-        return self.view.substr(self.view.line(self.view.text_point(row, 0)))
-
-    def test_partial_as(self):
-        setText(self.view, "import { FullName as f, createname as cr } from './createname'; // Partial")
-        self.view.run_command('edit_remove_unsed_imports', args=({'data': {"1":[{"line":1,"pos":22,"name":"f"}]}}))
-        first_row = self.getRow(0)
-        self.assertEqual(first_row, "import { createname as cr } from './createname'; // Partial")
-
-    def test_unused_all(self):
-        setText(self.view, "import {a, b, xx as c} from './createname';  // Unused all")
-        # line1 format of data which we are receiving from backend typescript check
-        line1 = [{"line":1,"name":"a"}, {"line":1,"name":"b"}, {"line":1,"name":"c"}]
-        self.view.run_command('edit_remove_unsed_imports', args=({'data': {"1":line1}}))
-        first_row = self.getRow(0)
-        self.assertEqual(first_row, "")
-
-    def test_unused_all_dollar_sign(self):
-        setText(self.view, "import {$x, x as $xx, $x as $xxx, $a as x$} from './goo';")
-        line1 = [{"line":1,"name":"$x"}, {"line":1,"name":"$xx"}, {"line":1,"name":"$xxx"}, {"line":1,"name":"x$"}]
-        self.view.run_command('edit_remove_unsed_imports', args=({'data': {"1":line1}}))
-        first_row = self.getRow(0)
-        self.assertEqual(first_row, "")
-
-    def test_unused_single_as(self):
-        setText(self.view, "import { Greeter as gr } from './greeter'; // Unused")
-        line1 = [{"line":1,"name":"gr"}]
-        self.view.run_command('edit_remove_unsed_imports', args=({'data': {"1":line1}}))
-        first_row = self.getRow(0)
-        self.assertEqual(first_row, "")
-
-    def test_used_should_not_be_removed(self):
-        setText(self.view, "import {a} from './greeter';\nimport { b } from './greeter';")
-        line1 = [{"line":1,"name":"a"}]
-        self.view.run_command('edit_remove_unsed_imports', args=({'data': {"1":line1}}))
-        first_row = self.getRow(0)
-        self.assertEqual(first_row, "import { b } from './greeter';")
-
-    def test_unused_import_all(self):
-        setText(self.view, "import * as someLib from 'prettier'; // Unused")
-        line1 = [{"line":1,"name":"someLib"}]
-        self.view.run_command('edit_remove_unsed_imports', args=({'data': {"1":line1}}))
-        first_row = self.getRow(0)
-        self.assertEqual(first_row, "")
-
-class TestPasteImport(TestCase):
-
-    def setUp(self):
-        self.view = sublime.active_window().new_file()
-        s = sublime.load_settings('Preferences.sublime-settings')
-        s.set('close_windows_when_empty', False)
-
-    def tearDown(self):
-        if self.view:
-            self.view.set_scratch(True)
-            self.view.window().focus_view(self.view)
-            self.view.window().run_command('close_file')
-
     def test_get_import_block(self):
         pass
 
-class TestExample(TestCase):
 
+class TestExample(TestCase):
     def setUp(self):
         self.view = sublime.active_window().new_file()
         # make sure we have a window to work with
@@ -294,10 +304,11 @@ class TestExample(TestCase):
 
     def test_smoke(self):
         self.assertTrue(True)
-        
+
     def test_hello_world(self):
         self.view.run_command("hello_world")
         first_row = self.getRow(0)
+
 
 def setText(view, string):
     view.run_command("select_all")
