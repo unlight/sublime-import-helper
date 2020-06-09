3fc346715c4e2788aea32eca5cb6519b6d014697
roman <>
2020-06-09 15:08:39 +0400

    ci: Semantic release config

--- .releaserc.js
+++ .releaserc.js
@@ -1,14 +1,25 @@
 module.exports = {
 	plugins: [
-		'@semantic-release/commit-analyzer',
-		'@semantic-release/release-notes-generator',
-		'@semantic-release/changelog',
-		'@semantic-release/github',
-		'@semantic-release/git',
 		[
 			'@semantic-release/exec',
 			{
 				prepareCmd: 'sh Taskfile prepare ${nextRelease.version}',
+			},
+		],
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/changelog',
+		'@semantic-release/github',
+		[
+			'@semantic-release/git',
+			{
+				assets: [
+					'messages.json',
+					'CHANGELOG.md',
+					'package.json',
+					'package-lock.json',
+					'npm-shrinkwrap.json',
+				],
 			},
 		],
 	],
